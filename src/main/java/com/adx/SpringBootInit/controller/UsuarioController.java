fde1856914c2a3e4a05a8781e89f78168aecbaeb
Thiago Martins <thiagolealmartins@gmail.com>
2024-11-27 17:21:25 -0300

    projeto completo

--- src/main/java/com/adx/SpringBootInit/controller/UsuarioController.java
+++ src/main/java/com/adx/SpringBootInit/controller/UsuarioController.java
@@ -1,11 +1,6 @@
 package com.adx.SpringBootInit.controller;
 
 import com.adx.SpringBootInit.bo.IUsuarioBo;
-import com.adx.SpringBootInit.exception.SenhaIncorretaException;
-import com.adx.SpringBootInit.exception.UsuarioNotFoundException;
-import com.adx.SpringBootInit.modal.dto.Error;
-import com.adx.SpringBootInit.modal.dto.UsuarioDto;
-import com.adx.SpringBootInit.modal.dto.UsuarioResponse;
 import com.adx.SpringBootInit.security.SecurityConfig;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -15,9 +10,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/usuario")
--- src/test/java/com/adx/SpringBootInit/dao/UsuarioDaoTest.java
+++ src/test/java/com/adx/SpringBootInit/dao/UsuarioDaoTest.java
@@ -1,20 +1,28 @@
 package com.adx.SpringBootInit.dao;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assumptions.*;
+import static org.mockito.Mockito.*;
 
+import com.adx.SpringBootInit.config.ApplicationConfig;
+import com.adx.SpringBootInit.enuns.RolesEnum;
+import com.adx.SpringBootInit.exception.UsuarioNotFoundException;
+import com.adx.SpringBootInit.modal.Usuario;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @DataJpaTest
-@ExtendWith(value = SpringExtension.class)
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 @ActiveProfiles(value = "teste")
 class UsuarioDaoTest {
@@ -22,5 +30,29 @@ class UsuarioDaoTest {
 	@Autowired
 	private IUsuarioDao dao;
 
+	private static BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+	private Usuario user;
+
+	@BeforeEach
+	public void setUp(){
+		user = Usuario.builder().email("teste@gmail.com").senha(encoder.encode("123")).nome("teste").role(RolesEnum.USUARIO).build();
+		dao.save(user);
+	}
+
+	@Test
+	public void existsByEmail(){
+		boolean result = dao.existsByEmail("teste@gmail.com");
+		assertTrue(result);
+	}
+
+	@Test
+	public void findByEmail(){
+		assertDoesNotThrow(() ->{
+			Usuario userAux = dao.findByEmail(user.getEmail()).orElseThrow(UsuarioNotFoundException::new);
+			assertThat(userAux).isNotNull();
+			assertEquals("teste", userAux.getNome());
+		});
+	}
 
 }
